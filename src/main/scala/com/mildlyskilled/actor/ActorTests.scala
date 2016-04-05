3bfcf40b4f6e38243ee76fb548dcc5a44191d892
Tom Lucas <tlucas03@dcs.bbk.ac.uk>
2016-04-05 14:47:52 +0100

    Tom commit test changes

---
+++ src/main/scala/com/mildlyskilled/actor/ActorTests.scala
@@ -0,0 +1,31 @@
+package com.mildlyskilled.actor
+
+/*//import akka.actor.Status.Success
+import akka.testkit.TestActorRef
+//import org.scalatest._
+
+
+import scala.concurrent.duration._
+import scala.concurrent.Await
+import akka.pattern.ask
+
+import scala.util.Success
+/**
+  * Created by Tom on 03/04/2016.
+  */
+class ActorTests {
+
+
+  val actorRef = TestActorRef[TracerActor]
+  val actor = actorRef.underlyingActor
+
+    val actorRef1 = TestActorRef(new TracerActor)
+    actorRef1 !
+    // hypothetical message stimulating a '42' answer
+    val future = actorRef1 ?
+    val Success(result:) = future.value.get
+    result should be()
+
+
+}
+*/
--- src/test/scala/TracerActorTests.scala
+++ src/test/scala/TracerActorTests.scala
@@ -2,31 +2,78 @@ package sample.akka.testkit
 
 import akka.actor.ActorSystem
 import akka.actor.Actor
-import akka.testkit.{TestKit, TestActorRef}
-import org.scalatest.matchers.MustMatchers
-import org.scalatest.WordSpec
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
+import org.scalatest.MustMatchers
+import org.scalatest.WordSpecLike
 import com.mildlyskilled._
-import com.mildlyskilled.actors._
+import com.mildlyskilled.actor.TracerActor
+import com.mildlyskilled.actor._
 import com.mildlyskilled.protocol._
 
+import scala.concurrent.duration._
+
 class TracerActorTests extends TestKit(ActorSystem("testSystem"))
-  with WordSpec
-  with MustMatchers {
+  with WordSpecLike
+  with MustMatchers
+  with ImplicitSender {
 
   val infile = "src/main/resources/input.dat"
   val scene = FileReader.parse(infile)
   val row = 0
-  val image = new Image(Trace.width, Trace.height)
-  
-  "A simple actor" must {
+  val image = new Image(Trace.Width, Trace.Height)
+  val color = Color.black
+
+  "A Tracer actor" must {
     // Creation of the TestActorRef
     val actorRef = TestActorRef[TracerActor]
 
-    "receive messages" in {
-      // This call is synchronous. The actor receive() method will be called in the current thread
-      actorRef ! TracerProtocol.TracePixels(scene, width, height, row)
+    "send a set message" in {
+      within(10000 millis) {
+        // This call is synchronous. The actor receive() method will be called in the current thread
+        actorRef ! TracerProtocol.TracePixels(scene, Trace.Width, Trace.Height, row)
+        // With actorRef.underlyingActor, we can access the SimpleActor instance created by Akka
+        expectMsg(CoordinatorProtocol.Set(0, 0, color))
+      }
+    }
+    "send 800 set messages" in {
+      actorRef ! TracerProtocol.TracePixels(scene, Trace.Width, Trace.Height, row)
       // With actorRef.underlyingActor, we can access the SimpleActor instance created by Akka
-      actorRef.underlyingActor.lastMsg must equal(CoordinatorProtocol.RequestMoreWork)
+      receiveN(800)
     }
   }
+
+ /* "A Tracer actor" must {
+    // Creation of the TestActorRef
+    val actorRef2 = TestActorRef[CoordinatorActor]
+
+    "send a set message" in {
+      within(10000 millis) {
+        // This call is synchronous. The actor receive() method will be called in the current thread
+        actorRef2 ! CoordinatorProtocol.TraceImage
+        // With actorRef.underlyingActor, we can access the SimpleActor instance created by Akka
+        expectMsg(TracerProtocol.TracePixels(scene, Trace.Width, Trace.Height, row))
+      }
+    }
+    "send a set message" in {
+      within(10000 millis) {
+        // This call is synchronous. The actor receive() method will be called in the current thread
+        actorRef2 ! CoordinatorProtocol.RequestMoreWork
+        // With actorRef.underlyingActor, we can access the SimpleActor instance created by Akka
+        expectMsg(TracerProtocol.TracePixels(scene, Trace.Width, Trace.Height, 802))
+      }
+    }
+    "send a set message" in {
+      within(10000 millis) {
+        // This call is synchronous. The actor receive() method will be called in the current thread
+        actorRef2 ! CoordinatorProtocol.Set(0, 0, Color.black)
+        // With actorRef.underlyingActor, we can access the SimpleActor instance created by Akka
+        expectNoMsg()
+      }
+    }
+    /*"send 800 set messages" in {
+      actorRef ! TracerProtocol.TracePixels(scene, Trace.Width, Trace.Height, row)
+      // With actorRef.underlyingActor, we can access the SimpleActor instance created by Akka
+      expectNoMsg
+    }*/
+  }*/
 }
